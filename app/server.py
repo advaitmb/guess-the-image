0e144b61afa7ebdb454a8a091aaec5828a562400
Advait Bhat <advaitmb@gmail.com>
2020-03-23 03:12:31 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/q9gt3rla8vegfiw/export.pkl'
+export_file_url = 'https://www.dropbox.com/s/q9gt3rla8vegfiw/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['dog','cat']
